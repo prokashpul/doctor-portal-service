859a3b435f6fba9c004837b596a24f9066b864bd
prokash <pprokash102@gmail.com>
2022-05-10 00:39:07 +0600

    footer section add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./Components/Sheared/Footer/Footer";
 import Header from "./Components/Sheared/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
